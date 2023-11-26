b26311d4e7cf612cb303169004a943ce26f08c1f
Jorge Obiols <jorge.obiols@gmail.com>
2023-11-26 11:11:09 -0300

    [ADD] so_out_refund

---
+++ so_out_refund/__manifest__.py
@@ -0,0 +1,34 @@
+# -----------------------------------------------------------------------------
+#
+#    Copyright (C) 2018  jeo Software  (http://www.jeosoft.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# -----------------------------------------------------------------------------
+{
+    'name': 'so_out_refund',
+    'version': '11.0.1.0.0',
+    'license': 'Other OSI approved licence',
+    'category': 'account',
+    'summary': 'permitir agregar notas de credito desde ventas',
+    'author': 'jeo Software',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/invoice_view.xml',
+    ],
+    'installable': True,
+}
